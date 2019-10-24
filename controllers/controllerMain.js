957c9a4eb0d9dd8c1576cfe797b5f23b5d277155
tylerhipke <tylerhipke@gmail.com>
2019-10-24 14:20:18 -0700

    start implementing handlebars

--- controllers/controllerMain.js
+++ controllers/controllerMain.js
@@ -1,5 +1,27 @@
 var express = require("express");
-
+var exphbs = require("express-handlebars");
+var axios = require("axios");
 var router = express.Router();
 
-var news = require("../models/index.js")
+var news = require("../models");
+
+var queryUrl =
+  "https://api.nytimes.com/svc/topstories/v2/home.json";
+
+router.get("/", function(req, res) {
+  axios
+    .get(queryUrl, {
+      params: {
+        "api-key" : process.env.NYT_API_KEY
+      }
+    })
+    .then(function(response) {
+      console.log(response);
+      exphbs.render("index");
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var mongoose = require("mongoose");
 require("dotenv").config();
 
-var PORT = process.env.PORT || 3000;
-
 var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -16,24 +16,26 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+
 mongoose.connect("mongodb://localhost/nyt_db");
 
-db.News.create({
-  headline: "test article",
-  summary: "summary goes here",
-  url: "http://www.google.com"
-})
-  .then(dbNews => {
-    console.log(dbNews);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// db.News.create({
+//   headline: "test article",
+//   summary: "summary goes here",
+//   url: "http://www.google.com"
+// })
+//   .then(dbNews => {
+//     console.log(dbNews);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/controllerMain.js");
 
 app.use(routes);
 
-app.listen(PORT => {
-  console.log("App on port " + PORT);
+app.listen(PORT, function(){
+    console.log("App running on port " + PORT + "!");
 });
+
