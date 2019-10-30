cc36e69b64ce6dad5b80ae03bae7d6993afd8ec5
tylerhipke <tylerhipke@gmail.com>
2019-10-30 12:19:21 -0700

    modify cheerio selectors

--- controllers/controllerDom.js
+++ controllers/controllerDom.js
@@ -1,11 +1,10 @@
-var cheerio = require("cheerio");
-var $ = cheerio.load('<h2 class="title">Hello world</h2>');
+// var $ = cheerio.load('<h2 class="title">Hello world</h2>');
 
-module.exports.drawArticles = function(data){
-    console.log("invoking cheerios");
-    $('h2.title').text('Hello there!')
-    $('h2').addClass('welcome')
-    $('.title').text()
-    //=> Hello there!    
-}
+// module.exports.drawArticles = function(data){
+//     console.log("invoking cheerios");
+//     $('h2.title').text('Hello there!')
+//     $('h2').addClass('welcome')
+//     $('.title').text()
+//     //=> Hello there!    
+// }
 
--- controllers/controllerMain.js
+++ controllers/controllerMain.js
@@ -1,17 +1,44 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var axios = require("axios");
+var cheerio = require("cheerio");
 var router = express.Router();
 var dom = require("./controllerDom.js");
 var db = require("../models");
+const fs = require("fs");
 
-var queryUrl = "https://api.nytimes.com/svc/topstories/v2/home.json";
+var queryUrl = "http://www.nytimes.com";
 
 router.get("/", function(req, res) {
   res.render("index");
 });
 
 router.get("/pull", function(req, res) {
-}
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      var $ = cheerio.load(response.data);
+      var siteContent = $(".css-6p6lnl");
+      var result = {};
+      $("article").each(function(i, element) {
+        console.log(
+          $(element)
+            .children(".assetWrapper")
+            .find(".css-zmhpb6")
+            
+        );
+        result.title = $(element)
+          .children(".assetWrapper")
+          .find(".css-zmhpb6")
+          .text();
+      });
 
-module.exports = Router;
+      console.log(result);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  res.render("index");
+});
+
+module.exports = router;
