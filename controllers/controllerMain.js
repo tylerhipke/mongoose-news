3bf4d864c8fa03fafdcd76e47bfbdaba0d177f19
tylerhipke <tylerhipke@gmail.com>
2019-10-30 12:53:46 -0700

    change from nyt to nbc sd and finish cheerio scraper

--- controllers/controllerMain.js
+++ controllers/controllerMain.js
@@ -7,10 +7,14 @@ var dom = require("./controllerDom.js");
 var db = require("../models");
 const fs = require("fs");
 
-var queryUrl = "http://www.nytimes.com";
+var queryUrl = "https://www.nbcsandiego.com/news/local/";
 
 router.get("/", function(req, res) {
   res.render("index");
+});
+
+router.get("/saved", function(req, res){
+  res.render("saved");
 });
 
 router.get("/pull", function(req, res) {
@@ -18,21 +22,29 @@ router.get("/pull", function(req, res) {
     .get(queryUrl)
     .then(function(response) {
       var $ = cheerio.load(response.data);
-      var siteContent = $(".css-6p6lnl");
+      var siteContent = $("#offlead");
       var result = {};
-      $("article").each(function(i, element) {
-        console.log(
-          $(element)
-            .children(".assetWrapper")
-            .find(".css-zmhpb6")
-            
-        );
-        result.title = $(element)
-          .children(".assetWrapper")
-          .find(".css-zmhpb6")
-          .text();
+      $(".article").each(function(i, element) {
+        var currentObj = {};
+        currentObj.title = $(element)
+          .children("div")
+          .children("p")
+          .children("a")
+          .attr("title");
+        currentObj.summary = $(element)
+          .children("div")
+          .children("p")
+          .children("a")
+          .attr("data-lid");
+        currentObj.link = $(element)
+          .children("div")
+          .children("p")
+          .children("a")
+          .attr("href");
+        if (currentObj.title && currentObj.summary && currentObj.link) {
+          result[i] = currentObj;
+        }
       });
-
       console.log(result);
     })
     .catch(function(error) {
