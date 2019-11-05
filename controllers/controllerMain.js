a444023240a34c29893773f0a68d534c41589945
tylerhipke <tylerhipke@gmail.com>
2019-11-05 11:22:36 -0800

    add client side event listener

--- controllers/controllerMain.js
+++ controllers/controllerMain.js
@@ -14,6 +14,7 @@ router.get("/", function(req, res) {
       var result = [];
       $(".article").each(function(i, element) {
         var currentObj = {};
+        currentObj.id = i;
         currentObj.title = $(element)
           .children("div")
           .children("p")
--- public/assets/webapp.js
+++ public/assets/webapp.js
@@ -1,1 +1,9 @@
 console.log("scripts loaded");
+
+document.addEventListener('click', function (event) {
+
+	if (!event.target.matches('.btn-save')) return;
+	event.preventDefault();
+	console.log(event.target);
+
+}, false);
